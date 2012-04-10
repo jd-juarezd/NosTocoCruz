0138c0ee1716699fada2d13ba7426964c210f502
Victor MA <tagoro9@gmail.com>
2012-04-10 11:17:01 +0100

    tests del modelo

--- social_djungle/User/features/steps.py
+++ social_djungle/User/features/steps.py
@@ -4,7 +4,28 @@ from django.test.client import Client
 from nose.tools import assert_equals
 
 @step(r'I create a sample user')
-def create_user(step):
-    u = User(email = "foo@bar.com", username="FooBar")
+def define_user(step):
+    u = User(email = "foo@bar.com", username="FooBar", password = "1234")
     world.user = u
+    assert 1 == 1
+    
+@step(r'It should be created correctly')
+def create_user(step):
+    #La clase User ha de tener un metodo que sea is_valid que determine
+    #si los campos son correctos
+    assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+
+@step(r'The "(.*)" cannot be empty')
+def empty_field(step,field):
+    world.user.field = ""
+assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+    
+@step(r'The "(.*)" should have the right format')
+def rigth_format(step,field):
+    world.user.field = "foo.@bar"
+assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+    
+
+
+    
     
