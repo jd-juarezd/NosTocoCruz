964d2cfae8d2b8e540af39814bbe0b308d21192f
Victor MA <tagoro9@gmail.com>
2012-04-10 13:44:04 +0100

    Tests modelo usuarios

--- social_djungle/User/features/model.feature
+++ social_djungle/User/features/model.feature
@@ -1,12 +1,18 @@
 Feature: Validating User Model
 
 	Scenario: Validating User Login
-		 When I create a sample user
-		 Then It should be created correctly
+		When I create a sample user
+		Then It should be created correctly
 		 
 	Scenario: Wrong email
 		When I create a sample user
 		Then The "email" cannot be empty
-		And The "email" should have the right format
-		And the "username" cannot be empty
+		And The "email" should not accept "foo.@bar."
+		
+	Scenario: User already exists
+		When I create a sample user
+		Then It should be created correctly
+		And saved
+		If I try to create it again
+		Then It should not be valid
 		
--- social_djungle/User/features/steps.py
+++ social_djungle/User/features/steps.py
@@ -7,25 +7,38 @@ from nose.tools import assert_equals
 def define_user(step):
     u = User(email = "foo@bar.com", username="FooBar", password = "1234")
     world.user = u
-    assert 1 == 1
+    assert isinstance(u,User)
     
 @step(r'It should be created correctly')
 def create_user(step):
-    #La clase User ha de tener un metodo que sea is_valid que determine
-    #si los campos son correctos
-    assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+    assert not User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
 
 @step(r'The "(.*)" cannot be empty')
 def empty_field(step,field):
     world.user.field = ""
-assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+    assert not User.validateData(world.user.username, world.user.email, world.user.password)
     
-@step(r'The "(.*)" should have the right format')
-def rigth_format(step,field):
-    world.user.field = "foo.@bar"
-assert User.exists(world.user.username) and User.validateData(world.user.username, world.user.email, world.user.password)
+@step(r'The "(.*)" should not accept "(.*)"')
+def rigth_format(step,field,word):
+    world.user.field = word
+    assert not User.validateData(world.user.username, world.user.email, world.user.password)
     
+@step(r'be saved')
+def save_user(step):
+    try:
+        world.user.save()
+    except ValidationError, e:
+        assert False
+    assert True
+    
+@step(r'I try to create it again')
+def create_user_again(step):
+    u = User(email = "foo@bar.com", username="FooBar", password = "1234")
+    world.user = u
+    assert isinstance(u,User)
 
-
+@step(r'Then It should not be valid')
+def validate_user(step):
+    assert User.exists(world.user.name)
     
     
