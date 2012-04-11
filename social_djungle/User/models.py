a7d93c6be8a777ba3a73f79a38c9a4f063010936
Alessandro Fanio González <alessandro.fg.27@gmail.com>
2012-04-11 20:27:16 +0100

    Empezando a crear el modelo de usuarios

--- social_djungle/User/models.py
+++ social_djungle/User/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=40) # SHA1 hash is 40 characters long
+    
+    def __unicode__(self):
+        return self.username
+    
+    @classmethod
+    def exists(cls, username):
+        res = cls.objects.filter(username=username)
+        if res:
+            return false
+        else:
+            return true
