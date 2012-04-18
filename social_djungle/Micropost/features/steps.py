0f39f4fce8a08ae44639cd47dad7b5c97f77691e
Jose Daniel <jdcuadrado3@gmail.com>
2012-04-18 13:49:11 +0100

    Actualizado micropost.features y empezamos con steps.py de micropost

--- social_djungle/Micropost/features/steps.py
+++ social_djungle/Micropost/features/steps.py
@@ -3,3 +3,9 @@ from lxml import html
 from django.test.client import Client
 from nose.tools import assert_equals
 from Micropost.models import Micropost
+
+@step(r'I create a sample micropost')
+def write_micropost(step):
+    m = Micropost(author = "1", text = "Hola Mundo")
+    world.micropost = m
+    assert isinstance(m,Micropost)
