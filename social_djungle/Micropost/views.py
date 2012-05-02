f5c4fc533ddbc7d2c1d7cbbf6cf7f1c28340d6e0
Alessandro Fanio González <alessandro.fg.27@gmail.com>
2012-05-02 13:48:26 +0100

    Añadida funcionalidad de publicar microposts a la caja de texto en el home

--- social_djungle/Micropost/views.py
+++ social_djungle/Micropost/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from Micropost.models import Microposts
+from User.models import Users
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template.loader import get_template
+from django.template import RequestContext
+import datetime
+
+def newMicropost(request):
+    if (request.session.get('id',False) and (Users.objects.filter(id=request.session['id']))): 
+        # If user has logged in
+        if Microposts.validateLength(request.POST['publication']):
+            user = Users.objects.get(id = request.session['id'])
+            m = Microposts(author = user,
+                           text = request.POST['publication'],
+                           date_post = datetime.datetime.now())
+            m.save()
+        return HttpResponseRedirect("/user/home")
+    else:
+        HttpResponseRedirect("/")
