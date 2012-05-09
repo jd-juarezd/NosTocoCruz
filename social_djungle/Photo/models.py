b4a52d9a14445c11ae4915ed2ccae5c45067b3f8
Toolo <barick2@gmail.com>
2012-05-09 13:45:49 +0100

    Reestablecido fichero fondo3.png

--- social_djungle/Photo/models.py
+++ social_djungle/Photo/models.py
@@ -2,8 +2,7 @@
 from django.db import models
 from User.models import Users
 
-
-
 class Photos(models.Model):
      image = models.ImageField(upload_to = 'photos')
      author = models.ForeignKey(Users)
+
