ad7c3b289eb5df45ff06831d343579478f42ff29
Toolo <barick2@gmail.com>
2012-05-09 13:36:42 +0100

    Tests de Photos terminados

--- social_djungle/Photo/features/steps.py
+++ social_djungle/Photo/features/steps.py
@@ -1,14 +1,44 @@
-Feature: Testing photo upload and access
+from lettuce import *
+from lxml import html
+from django.test.client import Client
+from nose.tools import assert_equals
+from User.models import Users,Friendships,ValidationError
+from Photo.models import Photos
 
-    Scenario: Uploading Photo
-    When I upload a photo
-    And Its extension is correct
-    Then It should be correctly stored
+
+# Feature: Testing photo upload and access
+
+@before.all
+def setupDB():
+    user = Users(email = "correo@correo.com", username="PhotoUser", password = "1234",
+                 name = "Someone", surname = "Somebody", birthdate = "1991-12-12",
+                 gender = "Hombre", timestamp = datetime.datetime.now())
+    user.saveUser()
+    world.user = user
+
+# Scenario: Uploading Photo
+@step(r'When I upload a photo')
+def upload_photo(step):
+    photo = Photos(image = 'photos/lion.png' , author = world.user)
+    photo.save() 
     
-    Scenario: Accessing Photo
-    When I retrieve a photo from the database
-    Then I can see it
+@step(r'Then I can access it')
+def get_photo(step):
+    query = Photos.objects.filter(author = world.user)
+    if (query):
+        world.photo = query.get()
+
+# Scenario: Resizing Photo
+@step(r'When I try to upload a big photo')
+def upload_big_photo(step):
+    photo = Photos(image = 'photos/fondo3.png', author = world.user)
+    world.photo = photo;
     
-    Scenario: Resizing Photo
-    When I upload a big photo
-    Then It is resized to a proper size
+@step(r'Then It is resized to a proper size')
+def resize_photo(step):
+    world.photo.resizePhoto()
+    assert ((world.photo.height_field <= 600) & (world.photo.width_field <= 800))
+
+@step(r'And It is correctly saved')
+def save(step):
+    world.photo.save()
